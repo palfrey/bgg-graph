a087792fcb21ba303c8dda0653cfb2811efc0890
Tom Parker <palfrey@tevp.net>
2017-02-27 00:16:11 +0000

    Add explicit paths for static files

--- bgg_graph/settings.py
+++ bgg_graph/settings.py
@@ -121,7 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join('graph', 'static'),
+)
 
 RABBIT_HOSTNAME = os.environ.get('RABBIT_PORT_5672_TCP', 'rabbit')
 
